09afb0f91dcf1131d05c604c449703242f421cd4
asdeed <141638718+asdeed@users.noreply.github.com>
2024-04-05 08:50:47 +0200

    Update lxc_set.sh - fix path lxc_attach

--- lxc_set.sh
+++ lxc_set.sh
@@ -307,7 +307,7 @@ msg_ok "Started LXC Container"
 #pct push $CTID vdub-install.sh /tmp/vdub-install.sh
 #lxc-attach -n $CTID -- chmod +x /tmp/vdub-install.sh && bash ./vdub-install.sh || exit
 #lxc-attach -n $CTID -- bash -c "$(wget -qLO - http://192.168.2.6/vdesk/vdub-install.sh)" || exit
-lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/asdeed/vdesk_pve/main/setup/$var_install.sh)" || exit
+lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/asdeed/vdesk_pve/main/$var_install.sh)" || exit
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
 pct set $CTID -description "# ${APP} LXC"
 msg_ok "Completed Successfully!\n"
