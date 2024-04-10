04ce3d3d4400a18a3975f5d8bb72703f37370313
Alexandre JAN <alexandre_jan@nodeswarm.eu>
2024-04-10 15:12:16 +0200

    oder setup pulseaudio

--- vdesk_deb-amd.sh
+++ vdesk_deb-amd.sh
@@ -213,33 +213,6 @@ systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
 msg_ok "Customized Container"
   fi
 
-msg_info "Setting up pulseaudio"
-sound_card=""
-for card_num in $(aplay -L | grep -oE '(surround[0-9]+|front|hdmi|hw|dmix|audio|usbstream|sysdefault|iec958|plughw):CARD=[^ ]*')
-do
-    if speaker-test -D $card_num -c 2 -l 1 > /dev/null 2>&1; then
-        sound_card=$card_num
-        break
-    fi
-done
-
-if [ -n "$sound_card" ]; then
-    msg_info "Setting up pulseaudio for sound card $sound_card"
-    echo "load-module module-alsa-sink device=$sound_card" >> /etc/pulse/default.pa
-    /bin/mkdir -p /home/vdkuser/.config/autostart/ 
-    cat << EOF | tee -a /home/vdkuser/.config/autostart/pulse.desktop
-[Desktop Entry]
-Type=Application
-Name=pulseaudio
-Exec=/usr/bin/pulseaudio
-StartupNotify=false
-Terminal=false
-EOF
-    msg_ok "Pulseaudio configured for sound card $sound_card"
-else
-    msg_error "Failed to detect a functional sound card. Pulseaudio configuration not set."
-fi
-msg_ok "Pulseaudio configured"
 
 msg_info "Setting up sunshine"
 wget https://raw.githubusercontent.com/asdeed/vdesk_pve/main/pkg/sunshine-debian-bookworm-amd64.deb &>/dev/null
@@ -249,6 +222,7 @@ udevadm control --reload-rules &>/dev/null
 #udevadm trigger &>/dev/null
 #modprobe uinput
 
+/bin/mkdir -p /home/vdkuser/.config/autostart/ 
 cat << EOF | tee -a /home/vdkuser/.config/autostart/sun.desktop
 [Desktop Entry]
 Type=Application
@@ -269,3 +243,32 @@ msg_info "Starting X up"
 systemctl start lightdm
 ln -fs /lib/systemd/system/lightdm.service /etc/systemd/system/display-manager.service
 msg_info "Started X"
+
+msg_info "Setting up pulseaudio"
+sound_card=""
+for card_num in $(aplay -L | grep -oE '(surround[0-9]+|front|hdmi|hw|dmix|audio|usbstream|sysdefault|iec958|plughw):CARD=[^ ]*')
+do
+    if speaker-test -D "$card_num" -c 2 -l 1 > /dev/null 2>&1; then
+        sound_card=$card_num
+        break
+    fi
+done
+
+if [ -n "$sound_card" ]; then
+    msg_info "Setting up pulseaudio for sound card $sound_card"
+    echo "load-module module-alsa-sink device=$sound_card" >> /etc/pulse/default.pa
+    cat << EOF | tee -a /home/vdkuser/.config/autostart/pulse.desktop
+[Desktop Entry]
+Type=Application
+Name=pulseaudio
+Exec=/usr/bin/pulseaudio
+StartupNotify=false
+Terminal=false
+EOF
+    /bin/chown vdkuser:vdkuser /home/vdkuser/.config/autostart/pulse.desktop
+    msg_ok "Pulseaudio configured for sound card $sound_card"
+else
+    msg_error "Failed to detect a functional sound card. Pulseaudio configuration not set."
+fi
+
+msg_ok "Pulseaudio configured"
